c947e12e7db49b1dcff093b9edc27713fbace0bb
VladimirSharldaev <vladimirsarldaev43246@gmail.com>
2022-10-13 20:34:54 +0300

    add example_001

---
+++ example_001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите число умноженное само на себя
+// пример: 4->16
+//-3->9
+//-7->49
+Console.WriteLine("Программа вычисления квадрата числа");
+Console.WriteLine("Введите число");
+//1. ВВод числа
+int input = int.Parse(Console.ReadLine()!);
+//2. вычисление квадрата числа
+int result = input * input;
+
+//3. Вывод результата на консоль
+Console.WriteLine("Квадрат числа " + result);
+//
+//Console.Writeline("Hello, world")
+//
